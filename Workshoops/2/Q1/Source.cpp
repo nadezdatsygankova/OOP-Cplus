bf9cb496cfcb580838c04418d5d3c23884879892
nadgenpr <nadia_tsygankova@mail.ru>
2020-05-27 09:39:08 -0400

    Update Source.cpp

--- Workshoops/2/Q1/Source.cpp
+++ Workshoops/2/Q1/Source.cpp
@@ -6,20 +6,20 @@ product, difference, and quotient of the two numbers.*/
 
 #include <iostream>
 
-using namespace std;
+
 
 int main() {
    // 
    float number1, number2;
-   cout << "Please enter the first number" << endl;//prompt user for data
-   cin >> number1;// read first float from user into number1
-   cout << "Please enter the first number" << endl;//prompt user for data
-   cin >> number2; //read second float from user into number2
+   std::cout << "Please enter the first number" << std:: endl;//prompt user for data
+   std:: cin >> number1;// read first float from user into number1
+   std::cout << "Please enter the first number" << std:: endl;//prompt user for data
+   std:: cin >> number2; //read second float from user into number2
 
-   cout << number1 + number2 << endl;//display sum
-   cout << number1 * number2 << endl;//display product
-   cout << number1 - number2 << endl;//display difference
-   cout << number1 / number2 << endl;//display quotient
+   std:: cout << number1 + number2 << std:: endl;//display sum
+   std:: cout << number1 * number2 << std:: endl;//display product
+   std:: cout << number1 - number2 << std:: endl;//display difference
+   std:: cout << number1 / number2 << std:: endl;//display quotient
 
    return 0;
 
